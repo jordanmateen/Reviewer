b7e2c505a0135b1e06cfce790f2c01ad600d2d21
Jordan Mateen <jordan.mateen@coxautoinc.com>
2021-08-31 20:17:57 -0400

    addinf server file

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+// const bodyParser = require('body-parser')
+
+const PORT = 2340;
+
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post('/postReview', (req, res)=>{
+    console.log(req.body)
+})
+
+app.listen(PORT, ()=>{
+    console.log(`HTTP port opened on ${PORT}....`)
+})
