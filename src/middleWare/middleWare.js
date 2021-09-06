52dc09f0c6e440dacf7f46983c1a2d79884e6e73
Jordan Mateen <jordan.mateen@coxautoinc.com>
2021-09-05 20:26:52 -0400

    stageing middle ware file

---
+++ src/middleWare/middleWare.js
@@ -0,0 +1,27 @@
+import * as actions from '../actions/actionCreators'
+import { FETCH_NAME, FETCH_NAME_FAILURE, FETCH_NAME_SUCCESS } from '../actions/constants';
+// import { SET_NAME } from '../actions/constants';
+
+const fetchName = (dispatch) => {
+    fetch('https://swapi.dev/api/people/12')
+    .then(res => res.json())
+    .then( res => {
+        const splitName = res.name.split(' ');
+        const firstName = splitName[0];
+        const lastName = splitName[1];
+        dispatch({type: FETCH_NAME_SUCCESS, data: {firstName,lastName}});
+    }).catch(error => {
+        dispatch({type: FETCH_NAME_FAILURE, data: error});
+    })
+}
+
+export const nameMiddleWare = ({dispatch}) => next => action => {
+    const { type } = action
+    switch(type) {
+        case FETCH_NAME :
+            fetchName(dispatch)
+            break;
+        default :
+            next(action)
+    }
+}
